71d2db4ed486a8e07e4152743583d144b5f896a2
NEBULA1978 <rpascualey@gmail.com>
2023-06-23 21:58:14 +0200

    Creando base de datos con mongodb

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import mysql from 'mysql2/promise'
+import mongoose from 'mongoose'
 
 const app = express()
 
@@ -12,6 +13,13 @@ const client = mysql.createPool({
 
 const response = await client.query('SELECT 1 + 1')
 console.log(response)
+
+// MOGODEB
+const mongoConnection= await mongoose.connect(
+    "mongodb://localhost:27017/twitchdb"
+)
+
+console.log(mongoConnection.connection.db.databaseName)
 
 app.listen(3000)
 console.log('Server om port 3000')
@@ -65,3 +73,37 @@ console.log('Server om port 3000')
 
 // Creo mongodb:
 //  docker run --name,e mymongo -p 27017:27017 mongo
+// Instalo:
+// npm i mongoose
+
+// SALIDA POR CONSOLA:
+// next@rases:~/docker-mysql-fazt$ node server.js 
+// [
+//   [ { '1 + 1': 2 } ],
+//   [
+//     ColumnDefinition {
+//       _buf: <Buffer 01 00 00 01 01 1b 00 00 02 03 64 65 66 00 00 00 05 31 20 2b 20 31 00 0c 3f 00 03 00 00 00 08 81 00 00 00 00 05 00 00 03 fe 00 00 02 00 02 00 00 04 01 ... 10 more bytes>,
+//       _clientEncoding: 'utf8',
+//       _catalogLength: 3,
+//       _catalogStart: 10,
+//       _schemaLength: 0,
+//       _schemaStart: 14,
+//       _tableLength: 0,
+//       _tableStart: 15,
+//       _orgTableLength: 0,
+//       _orgTableStart: 16,
+//       _orgNameLength: 0,
+//       _orgNameStart: 23,
+//       characterSet: 63,
+//       encoding: 'binary',
+//       name: '1 + 1',
+//       columnLength: 3,
+//       columnType: 8,
+//       type: 8,
+//       flags: 129,
+//       decimals: 0
+//     }
+//   ]
+// ]
+// twitchdb
+// Server om port 3000
