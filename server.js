cef4d3331849b6b06332ebee3e508578639ea39a
NEBULA1978 <rpascualey@gmail.com>
2023-08-31 19:21:29 +0200

    Añadiendo pruebas

--- server.js
+++ server.js
@@ -24,6 +24,104 @@ console.log(mongoConnection.connection.db.databaseName)
 app.listen(3000)
 console.log('Server om port 3000')
 
+// Crear un nuevo usuario en la base de datos MySQL
+// app.post('/mysql/users', async (req, res) => {
+//     try {
+//         const { username, email } = req.body;
+//         await client.query('INSERT INTO users (username, email) VALUES (?, ?)', [username, email]);
+//         res.status(201).send('User created');
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send('Error creating user');
+//     }
+// });
+
+// // Obtener todos los usuarios de la base de datos MySQL
+// app.get('/mysql/users', async (req, res) => {
+//     try {
+//         const [users] = await client.query('SELECT * FROM users');
+//         res.json(users);
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send('Error fetching users');
+//     }
+// });
+
+// // Actualizar información de un usuario en la base de datos MySQL
+// app.put('/mysql/users/:id', async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         const { username, email } = req.body;
+//         await client.query('UPDATE users SET username = ?, email = ? WHERE id = ?', [username, email, id]);
+//         res.send('User updated');
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send('Error updating user');
+//     }
+// });
+
+// // Eliminar un usuario de la base de datos MySQL
+// app.delete('/mysql/users/:id', async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         await client.query('DELETE FROM users WHERE id = ?', [id]);
+//         res.send('User deleted');
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send('Error deleting user');
+//     }
+// });
+
+
+// // Crear un nuevo usuario en la base de datos MongoDB
+// app.post('/mongodb/users', async (req, res) => {
+//     try {
+//         const { username, email } = req.body;
+//         const newUser = new User({ username, email });
+//         await newUser.save();
+//         res.status(201).send('User created');
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send('Error creating user');
+//     }
+// });
+
+// // Obtener todos los usuarios de la base de datos MongoDB
+// app.get('/mongodb/users', async (req, res) => {
+//     try {
+//         const users = await User.find();
+//         res.json(users);
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send('Error fetching users');
+//     }
+// });
+
+// // Actualizar información de un usuario en la base de datos MongoDB
+// app.put('/mongodb/users/:id', async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         const { username, email } = req.body;
+//         await User.findByIdAndUpdate(id, { username, email });
+//         res.send('User updated');
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send('Error updating user');
+//     }
+// });
+
+// // Eliminar un usuario de la base de datos MongoDB
+// app.delete('/mongodb/users/:id', async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         await User.findByIdAndDelete(id);
+//         res.send('User deleted');
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send('Error deleting user');
+//     }
+// });
+
 // Creo msql:
 // docker run --name projectmsql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=fastpassword mysql
 
@@ -72,7 +170,7 @@ console.log('Server om port 3000')
 // //////////////////////////////////
 
 // Creo mongodb:
-//  docker run --name,e mymongo -p 27017:27017 mongo
+//  docker run --name mymongo -p 27017:27017 mongo
 // Instalo:
 // npm i mongoose
 
@@ -107,3 +205,10 @@ console.log('Server om port 3000')
 // ]
 // twitchdb
 // Server om port 3000
+
+// En localhost:
+// It looks like you are trying to access MongoDB over HTTP on the native driver port.
+
+// http://localhost:3306/
+// I���
+// 8.1.0�	���NBI{0AX�ےےے�ےك����������`\+Fe6Or~�caching_sha2_password�!��ے„#08S01Got packets out of order
